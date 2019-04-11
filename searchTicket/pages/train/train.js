5e7a46892da03bccc4ea1e381c609254e0ef2a59
晓锟 <anker.huang@scysun.com>
2019-04-11 18:16:29 +0800

    首页完成一小半

--- searchTicket/pages/train/train.js
+++ searchTicket/pages/train/train.js
@@ -5,14 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    imgUrls: [
+      '/images/haibao/1.jpg',
+      '/images/haibao/2.jpg',
+      '/images/haibao/3.jpg'
+    ],
+    currentTab: 0,
+    currentDate: "2019-04-11",
+    dateNow: "",
+    currentDate: ""
+  },
 
+  getNowDate: function () {
+    var date = new Date();
+    var dateStr = [date.getFullYear(), date.getMonth()+1, date.getDate()].join('-');
+    console.log("日期：" + dateStr);
+    return dateStr;
+  },
+
+  switchNav: function (e) {
+    var id = e.currentTarget.id;
+    this.setData({currentTab: id});
+  },
+
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      currentDate: e.detail.value
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      dateNow: this.getNowDate(),
+      currentDate: this.getNowDate()
+    });
   },
 
   /**
